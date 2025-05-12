688075b77ac9d920a0799e3cfcabaa0edebdcacb
Dan Yonezawa <dan@dzway.io>
2025-05-12 17:42:20 -0400

    v0.0.2 binaries fix and get-digit.sh updated

--- get-digit.sh
+++ get-digit.sh
@@ -3,8 +3,8 @@
 # download digit
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     wget --no-check-certificate --content-disposition https://bintray.com/dzway/digit/download_file?file_path=linux_amd64%2Fdigit%2Fdigit;;
-    Darwin*)    wget --no-check-certificate --content-disposition https://bintray.com/dzway/digit/download_file?file_path=darwin%2Fdigit%2Fdigit;;
+    Linux*)     wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/dzway/digit/blob/master/bin/linux/digit;;
+    Darwin*)    wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/dzway/digit/blob/master/bin/darwin/digit;;
     *)          echo "Sorry, there's no available package for your OS, contact github.com/dzway/digit."
 esac
 
